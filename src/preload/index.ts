1a1025cc3cc11d2af69d9b481f7d73803a9f3fe1
mahirminhajk <kmahirminhaj@gmail.com>
2024-08-28 23:16:46 +0530

    initial: project setup

---
+++ src/preload/index.ts
@@ -0,0 +1,11 @@
+import { contextBridge } from 'electron'
+
+if (!process.contextIsolated) {
+  throw new Error('contextIsolation must be enabled in the BrowserWindow')
+}
+
+try {
+  contextBridge.exposeInMainWorld('electron', {})
+} catch (error) {
+  console.log(error)
+}
